af312a9d0ebf1ee956d210118c084235a329c23f
karolbarkowski <karol.barkowski@gmail.com>
2025-08-25 00:34:52 +0200

    animation added

--- src/app/(frontend)/(page-components)/HowCanYouHelp/HowCanYouHelp.tsx
+++ src/app/(frontend)/(page-components)/HowCanYouHelp/HowCanYouHelp.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 'use client'
 import React from 'react'
+import { Slide, Fade } from 'react-awesome-reveal'
 
 const HowCanYouHelp = () => {
   const Features = [
@@ -22,28 +23,31 @@ const HowCanYouHelp = () => {
   ]
 
   return (
-    <div className="py-16 bg-[#f7f7f7]" id="how-can-you-help">
-      <div className="container mx-auto flex flex-col gap-12">
+    <div className="bg-[#f7f7f7] py-16" id="how-can-you-help">
+      <div className="flex flex-col gap-12 mx-auto container">
         <div className="flex flex-col gap-4 text-center">
-          <span className="items-center text-[#eb9309] uppercase fw-600 text-sm tracking-widest">
-            <i className="fa-solid fa-heart mr-2 animate-[scale-up-one_2s_infinite]"></i>
+          <span className="items-center text-[#eb9309] text-sm uppercase tracking-widest fw-600">
+            <i className="mr-2 animate-[scale-up-one_2s_infinite] fa-solid fa-heart"></i>
             potrzebujemy Cię
           </span>
           <h2>Jak możesz pomóc?</h2>
         </div>
-        <div className="flex flex-row between gap-16">
-          {Features.map((features, fitem) => (
-            <div className="col-lg-4 col-md-6 col-12 bg-white p-10 pt-18 relative" key={fitem}>
-              <img
-                src={features.icon}
-                className="absolute top-[-41px] shadow-xs rounded-full bg-white border-[#f1f1f1] border-1 p-5 hover:border-[#eb9309] transition-colors duration-300"
-              />
-              <div className="keyFeatureBlock__content">
-                <h3>{features.title}</h3>
-                <p className="keyFeatureBlock__text">{features.des}</p>
+
+        <div className="flex flex-row gap-16 between">
+          <Fade cascade damping={0.2} triggerOnce={true}>
+            {Features.map((features, fitem) => (
+              <div className="relative bg-white p-10 pt-18 col-lg-4 col-md-6 col-12">
+                <img
+                  src={features.icon}
+                  className="top-[-41px] absolute bg-white shadow-xs p-5 border-[#f1f1f1] border-1 hover:border-[#eb9309] rounded-full transition-colors duration-300"
+                />
+                <div className="keyFeatureBlock__content">
+                  <h3>{features.title}</h3>
+                  <p className="keyFeatureBlock__text">{features.des}</p>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </Fade>
         </div>
       </div>
     </div>
